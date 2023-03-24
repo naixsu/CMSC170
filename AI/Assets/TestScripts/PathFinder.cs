b402c1ae7f1b069630951cfcd8a35c8099c936db
naixsu <plnacua02@gmail.com>
2023-03-24 13:30:03 +0800

    Added pathfinding using mouse3
    Pathfinding works on all tiles atm, not tilled tiles.

---
+++ AI/Assets/TestScripts/PathFinder.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathFinder
+{
+    public List<OverlayTile> FindPath(OverlayTile start, OverlayTile end)
+    {
+        List<OverlayTile> openList = new List<OverlayTile>();
+        List<OverlayTile> closedList = new List<OverlayTile>();
+
+        openList.Add(start);
+
+        while (openList.Count > 0)
+        {
+            OverlayTile currentOverlayTile = openList.OrderBy(x => x._F).First();
+
+            openList.Remove(currentOverlayTile);
+            closedList.Add(currentOverlayTile);
+
+            if (currentOverlayTile == end)
+            {
+                // finalize our path once goal is found
+
+                return GetFinishedList(start ,end);
+
+            }
+
+            var neighborTiles = GetNeighborTiles(currentOverlayTile);
+
+            foreach (var neighbor in neighborTiles)
+            {
+                // check valid neighbors
+                if (neighbor.isBlocked || closedList.Contains(neighbor))
+                {
+                    continue;
+                }
+
+                // calculate G value
+                neighbor._G = GetManhattanDistance(start, neighbor);
+                // calculate H value
+                neighbor._H = GetManhattanDistance(end, neighbor);
+
+                neighbor.previous = currentOverlayTile;
+
+                if (!openList.Contains(neighbor))
+                {
+                    openList.Add(neighbor);
+                }
+
+            }
+        }
+
+        return new List<OverlayTile>();
+    }
+
+    private List<OverlayTile> GetFinishedList(OverlayTile start, OverlayTile end)
+    {
+        List<OverlayTile> finishedList = new List<OverlayTile>();
+
+        OverlayTile currentTile = end;
+
+        while (currentTile != start)
+        {
+            finishedList.Add(currentTile);
+            currentTile = currentTile.previous;
+        }
+
+        finishedList.Reverse();
+
+        return finishedList;
+    }
+
+    private int GetManhattanDistance(OverlayTile start, OverlayTile neighbor)
+    {
+        return Mathf.Abs(start.gridLocation.x - neighbor.gridLocation.x) + Mathf.Abs(start.gridLocation.y - neighbor.gridLocation.y);
+    }
+
+    private List<OverlayTile> GetNeighborTiles(OverlayTile currentOverlayTile)
+    {
+        var map = MapManager.Instance.map;
+
+        List<OverlayTile> neighbors = new List<OverlayTile>();
+
+        #region GET NEIGHBORS
+
+        // top
+        Vector2Int locationToCheck = new Vector2Int(
+            currentOverlayTile.gridLocation.x,
+            currentOverlayTile.gridLocation.y + 1
+            );
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        // down
+        locationToCheck = new Vector2Int(
+            currentOverlayTile.gridLocation.x,
+            currentOverlayTile.gridLocation.y - 1
+            );
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        // left
+        locationToCheck = new Vector2Int(
+            currentOverlayTile.gridLocation.x - 1,
+            currentOverlayTile.gridLocation.y
+            );
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        // right
+        locationToCheck = new Vector2Int(
+            currentOverlayTile.gridLocation.x + 1, 
+            currentOverlayTile.gridLocation.y
+            );
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        #endregion
+
+        return neighbors;
+    }
+}
